fb76314d5d002f09f070a9f39212a3da74e7aeb6
Bruno Scherrer <bruno.scherrer@inria.fr>
2020-10-12 17:53:33 +0200

    first commit

---
+++ generate.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+from covid import *
+
+
+generate_graphs()
+
+generate_markdown()
+
+#generate_anim()
---
+++ update_files.sh
@@ -0,0 +1,7 @@
+rm -f time_series_* UID_ISO_FIPS_LookUp_Table.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv
